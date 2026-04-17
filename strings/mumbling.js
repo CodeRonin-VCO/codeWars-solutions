26df3438fcd98f1ceeb1ec55e3ebd358d3f6a004
CodeRonin <vincentcss16@gmail.com>
2026-04-17 17:58:03 +0200

    Mumbling — 7kyu

---
+++ strings/mumbling.js
@@ -0,0 +1,20 @@
+// Kata : Mumbling — 7kyu
+// Objectif : accum("abcd") -> "A-Bb-Ccc-Dddd"
+
+function accum(str) {
+	return str
+    .split("")
+    .map((l, i) => {
+        return l.toUpperCase() + l.toLowerCase().repeat(i);
+    })
+    .join("-");
+}
+
+
+console.log("-------------");
+console.log(accum("ZpglnRxqenU")); // "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+console.log(accum("NyffsGeyylB")); // "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+console.log(accum("MjtkuBovqrU")); // "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+console.log(accum("EvidjUnokmM")); // "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+console.log(accum("HbideVbxncC")); // "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
+console.log("-------------");
