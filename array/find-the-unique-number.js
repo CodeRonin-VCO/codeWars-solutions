de5cc75292209edb94eb26f10cb76cf6757980a0
CodeRonin <vincentcss16@gmail.com>
2026-04-20 17:38:33 +0200

    Find the unique number — 6kyu

---
+++ array/find-the-unique-number.js
@@ -0,0 +1,18 @@
+// Kata : Find the unique number — 6kyu
+// Objectif : trouver le seul nombre qui est différent des autres
+
+function findUniq(arr) {
+  return arr.find(n => arr.indexOf(n) === arr.lastIndexOf(n));
+}
+
+
+
+
+console.log("-------------");
+console.log(findUniq([ 1, 0, 0 ])); // 1
+console.log(findUniq([ 0, 1, 0 ])); // 1
+console.log(findUniq([ 0, 0, 1 ])); // 1
+console.log(findUniq([ 1, 1, 1, 2, 1, 1 ])); // 2
+console.log(findUniq([ 1, 1, 2, 1, 1 ])); // 2
+console.log(findUniq([ 3, 10, 3, 3, 3 ])); // 10
+console.log("-------------");
